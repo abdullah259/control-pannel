4458a492b5900a6ff86f65aa612ab03367329dd2
AbdullahGhazi <abdullahghazi404@gmail.com>
2021-04-25 22:21:01 -0700

    the chart is not appear

--- src/assets/js/chart.js
+++ src/assets/js/chart.js
@@ -1,20 +1,71 @@
 import "../../../node_modules/chart.js/dist/chart.min.js";
 
-(function () {
-    var ctx = document.getElementById('example-chart').getContext('2d');
-    var chart = new Chart(ctx, {    
-        type: 'line',
+const chartCanvas= document.getElementById("example-chart");
+const data= JSON.parse(chartCanvas.parentElement.dataset.values);
+const brandColor= (window.getComputedStyle(chartCanvas)).getPropertyValue("--color-brand")
+const chart = new Chart(chartCanvas, {
+    type: "line",
+    data: {
+        labels: ['January','February','March','April','May','June',],
+        datasets: [{
+            label: "مبيعات الشهر",
+            data: data,
+            borderColor: brandColor,
+            backgroundColor: "transparent",
+            lineTension: 0.2
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                display: false
+            }],
+            xAxes: [{
+                position:"top"
+            }]
+        }
+    }
+})
 
-        datasets: {
+
+(function(){
+    const chartCanvas= document.getElementById("example-chart");
+    const data= JSON.parse(chartCanvas.parentElement.dataset.values);
+    const brandColor= (window.getComputedStyle(chartCanvas)).getPropertyValue("--color-brand")
+    const chart = new Chart(chartCanvas, {
+        type: "line",
+        data: {
             labels: ['January','February','March','April','May','June',],
             datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45],
+                label: "مبيعات الشهر",
+                data: data,
+                borderColor: brandColor,
+                backgroundColor: "transparent",
+                lineTension: 0.2
             }]
         },
-
-        options: {}
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    display: false
+                }],
+                xAxes: [{
+                    position:"top"
+                }]
+            }
+        }
     });
+
+    const navigation= document.querySelector(".c-table-navigation");
+    const randomArray= (mylength, max) => Array.from({length: mylength}, () => Math.round(Math.random()*max));
+    navigation.addEventListener("click", () => {
+        chart.data.dataset[0].data= randomArray(12,1200);
+        chart.update();
+    })
 })();
